5ef353d366f617db9e80f8e2673d616d7c394cd0
dzl71 <davidzlatin406@gmail.com>
2024-09-10 00:03:41 +0300

    saving initial keybindings

---
+++ keybindings.lua
@@ -0,0 +1,29 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.keys = {
+	{
+		mods = "SUPER",
+		key = ';',
+		action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+	},
+	{
+		mods = "SUPER",
+		key = '"',
+		action = wezterm.action.SplitVertical({ domain = 'CurrentPaneDomain' }),
+	},
+	{
+		mods = "CTRL|ALT|SHIFT",
+		key = ':',
+		action = wezterm.action.SplitHorizontal({ domain = 'CurrentPaneDomain' }),
+	},
+	{
+		mods = "CTRL|ALT|SHIFT",
+		key = 'A',
+		action = wezterm.action.PaneSelect({
+			mode = "Activate",
+		}),
+	},
+}
+
+return config
